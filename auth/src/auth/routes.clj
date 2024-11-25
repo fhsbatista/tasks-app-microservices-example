e8a815685b197f5fb6660f1791ea03cadb8ffd6f
Fernando Batista <fernandohsbatista@gmail.com>
2024-11-25 17:08:21 -0300

    (auth):feat - signin api

--- auth/src/auth/routes.clj
+++ auth/src/auth/routes.clj
@@ -1,1 +1,55 @@
-(ns auth.routes)
+(ns auth.routes
+  (:require [io.pedestal.http.route :as route]
+            [io.pedestal.http :as http]
+            [io.pedestal.http.body-params :refer [body-params]]
+            [clojure.data.json :as json]
+            [auth.auth :as auth]))
+
+(def json-response-interceptor
+  {:name  ::json-response
+   :leave (fn [context]
+            (let [response (:response context)
+                  headers (merge (:headers response {})
+                                 {"Content-Type" "application/json;charset=UTF-8"})]
+              (if (:body response)
+                (assoc context :response
+                               (-> response
+                                   (update :body json/write-str)
+                                   (assoc :headers headers)))
+                context)))})
+
+(defn signin [context]
+  (let [params (:json-params context)
+        username (:username params)
+        password (:password params)
+        jwt (auth/authenticate username password)]
+    (if (= jwt nil)
+      {:status 400 :body {:message "Invalid credentials"}}
+      {:status 200 :body {:token jwt}})))
+
+(def routes
+  (route/expand-routes
+    #{["/signin" :post [json-response-interceptor (body-params) signin] :route-name :signin]}))
+
+(def service
+  {::http/routes routes
+   ::http/type   :jetty
+   ::http/port   8081})
+
+(defonce server (atom nil))
+
+(defn start-server []
+  (println "Starting server")
+  (reset! server
+          (http/start (http/create-server
+                        (assoc service ::http/join? false))))
+  (println "Server started"))
+
+(defn stop-server []
+  (http/stop @server))
+
+(defn restart-server []
+  (stop-server)
+  (start-server))
+
+;(restart-server)
