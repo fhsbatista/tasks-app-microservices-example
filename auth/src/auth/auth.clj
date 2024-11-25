676cef4da97a8155dad69affdacced38de7e47a2
Fernando Batista <fernandohsbatista@gmail.com>
2024-11-25 11:31:49 -0300

    (auth):feat - ensure authenticate function returns jwt

--- auth/src/auth/auth.clj
+++ auth/src/auth/auth.clj
@@ -1,10 +1,19 @@
-(ns auth.auth)
+(ns auth.auth
+  (:require [buddy.sign.jwt :as jwt]))
 
-(defn valid-password? [user password]
-  (and (= user "user") (= password "correct-password")))
+(def jwt-secret-key "secret-key")
+
+(defn- valid-password? [user password]
+  (and (= user "user")
+       (= password "correct-password")))
+
+(defn- generate-jwt [user]
+  (jwt/sign {:sub user
+             :iat (System/currentTimeMillis)}
+            jwt-secret-key))
 
 (defn authenticate [user password]
   (if (valid-password? user password)
-    "alkdsjfiqwenfranciusdfasndfsdaf"
+    (generate-jwt user)
     nil))
 
--- auth/test/auth/auth_test.clj
+++ auth/test/auth/auth_test.clj
@@ -1,12 +1,12 @@
 (ns auth.auth-test
-  (:require [clojure.test :refer :all])
-  (:require [auth.auth :refer :all]))
-
-(deftest test-valid-password?
-  (testing "When password is not valid"
-    (is (false? (valid-password? "user" "incorrect-password")))))
+  (:require [clojure.test :refer :all]
+            [auth.auth :refer :all]
+            [buddy.sign.jwt :as jwt]))
 
 (deftest test-authenticate
-  (testing "Returns token when password is valid"
-    (is (= "alkdsjfiqwenfranciusdfasndfsdaf"
-           (authenticate "user" "correct-password")))))
+  (testing "Returns valid jwt when password is valid"
+    (let [secret-key auth.auth/jwt-secret-key
+          token (authenticate "user" "correct-password")
+          unsigned (jwt/unsign token secret-key)
+          unsigned-user (:sub unsigned)]
+      (is (= unsigned-user "user")))))
