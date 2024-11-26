e74ac2c0948f8c82f0d64e5c8e1f8d86f071cc7e
Fernando Batista <fernandohsbatista@gmail.com>
2024-11-26 10:02:09 -0300

    (register):chore - install dependencies

--- register/project.clj
+++ register/project.clj
@@ -3,5 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.datomic/peer "1.0.7260"]
+                 [io.pedestal/pedestal.service "0.7.1"]
+                 [io.pedestal/pedestal.route "0.7.1"]
+                 [io.pedestal/pedestal.jetty "0.7.1"]
+                 [org.slf4j/slf4j-simple "1.7.28"]
+                 [org.clojure/data.json "2.5.0"]]
   :repl-options {:init-ns register.core})
